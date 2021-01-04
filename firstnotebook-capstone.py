772b72f49dad48934bceee8b85fe8866c16cb17a
hiralkn <76962717+hiralkn@users.noreply.github.com>
2021-01-04 23:05:42 +0530

    Add files via upload

---
+++ firstnotebook-capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook is part of my capstone project
+
+# ## It is part of my IBM certification
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello capstone project course!")
+
+
+# In[ ]:
+
+
+
+
